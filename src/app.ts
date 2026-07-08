f8b25c6e750c42cf1723fb669d5cf49cf1ee202b
Mahir4098 <mahirjr98@gmail.com>
2026-07-08 22:27:13 +0600

    Implement catchAsync and sendResponse function for code reusability

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
-import express, { type Application, type Request, type Response } from "express";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import cookieParser from "cookie-parser";
+import { authRoute } from "./module/auth.route";
 
 export const app: Application = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome To The Fixit Now Server");
